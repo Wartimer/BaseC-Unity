65cd8a4086395581860468b952dada4bbc92da7b
Wartimer <danryzhkov@mail.ru>
2021-10-18 16:00:35 +0300

    Обновление ветки

--- Lesson3_hw/RollABall/Player/TrapsSpawner.cs
+++ Lesson3_hw/RollABall/Player/TrapsSpawner.cs
@@ -15,9 +15,17 @@ namespace RollABall.Player
         private void Start()
         {
             spawner._spawnPoints = GameObject.FindGameObjectsWithTag("TrapSpawnPoint").ToList();
+
             
             foreach (var o in spawner._spawnPoints)
             {
+                if (o == null)
+                {
+                    throw new NullReferenceException("There is no points in List");
+                }
+
+                if (spawner._trapsPrefs == null) throw new NullReferenceException($"There is no Trap Prefabs in List of {nameof(spawner)}");
+                
                 var go = (GameObject) spawner._trapsPrefs[Random.Range(0, spawner._trapsPrefs.Count)].GetComponent<ICloneable>().Clone();
                 go.transform.position = o.transform.position;
                 go.transform.parent = o.transform;
